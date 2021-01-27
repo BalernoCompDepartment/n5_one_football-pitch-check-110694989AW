348c781a51985b4a81fe30431a643d0550b55932
110694989AW <110694989@ea.edin.sch.uk>
2021-01-27 12:04:11 +0000

    Wrote the program

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int length = Keyboard.getInt("what is the length of the pitch?");
+    
+    while(length < 90 || length > 120) {
+      Screen.display("that is invalid", "error screen");
+      length = Keyboard.getInt("what is the length of the pitch");
+    }
+    int width = Keyboard.getInt("what is the width of the pitch?");
+    
+    while(width < 45 || width > 90) {
+      Screen.display("that is invalid", "error Screen");
+      width = Keyboard.getInt("what is the width of the pitch?");
+    }
+    
+    String output = "the dimensions of length " + length + " and width " + width + " are valid";
+    
+    Screen.display(output, "validity check");
   }
 }
